bf8a205227054041fee58460be137175d34c7231
jolllan <jolllan@jolllan-pc>
2023-02-02 22:50:14 +0100

    Ajout couleur

--- download.sh
+++ download.sh
@@ -14,9 +14,16 @@
 
 file=yt-dlp_linux_aarch64
 
-echo "###########################################"
+
+echo "  ____  _           _     "
+echo " |  _ \| |      ___| |__  "
+echo " | | | | |     / __| '_ \ "
+echo " | |_| | |___ _\__ \ | | |"
+echo " |____/|_____(_)___/_| |_|"
+
+
 echo ""
-echo "Bienvenue, avec ce programme vous pouvez télécharger la musique de votre choix en mp3 (les playlists fonctionnent aussi)"
+echo "Bienvenue, avec ce programme vous pouvez télécharger la musique de votre choix en mp3 de la meilleure qualité possible(les playlists fonctionnent aussi)"
 echo ""
 
 # Vérifie si l'exécutable existe
@@ -24,7 +31,7 @@ if [ -e $file ]
 then
     echo ""
 else
-    echo "Je ne trouve pas le fichier 'yt-dlp_linux_aarch64' voulez vous que je le télécharge ? Y or N"
+    echo -e "\E[32m Je ne trouve pas le fichier 'yt-dlp_linux_aarch64' voulez vous que je le télécharge ? Y or N \E[0m"
     read -p "  > " condition
     
     # Télécharge ou non yt-dlp
@@ -32,7 +39,7 @@ else
     then
         wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux_aarch64
     else
-    echo "bah ciao alors"
+    echo -e "\E[31m Sortie de programme \E[0m"
     exit
     fi
 fi
@@ -43,22 +50,31 @@ if [[ -x "$file" ]]
 then
     echo ""
 else
-    echo "Le programme '$file' à besoin d'être exécutable voulez vous le faire par ce script ? Y or N"
+    echo -e "\E[32m Le programme '$file' à besoin d'être exécutable voulez vous le faire par ce script ? Y or N \E[0m"
     read -p "  > " consent
     if [ $consent == "Y" ]
     then 
         sudo chmod +x $file
     else 
-    echo "Erreur, le fichier doit pouvoir être éxécuter"
+    echo -e "\E[31m Erreur, le fichier doit pouvoir être éxécuter \E[0m"
     exit
     fi
 fi
 
 
 
-echo  "Entrez l'url"
+echo -e "\E[36m Entrez l'url \E[0m"
 read -p "  > " url
 echo "####"
-echo "Téléchargement en cours..."
+printf "Téléchargement en cours"
+sleep 0.2
+printf "."
+sleep 0.2
+printf "."
+sleep 0.2
+echo "."
+
 ./$file -f 'ba' $url -x --audio-format mp3 
-echo "Bye bye"
+echo -e "\E[36m Bye bye \E[0m"
+
+exit
